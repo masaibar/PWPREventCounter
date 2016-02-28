0cdbf661f1233347d4873998052dc89560df7131
masaibar <orebatemoko@gmail.com>
2016-02-28 21:53:08 +0900

    id追加

--- app/src/main/java/com/masaibar/eventbeforeaftercounter/HighSchool.java
+++ app/src/main/java/com/masaibar/eventbeforeaftercounter/HighSchool.java
@@ -7,6 +7,9 @@ import com.google.gson.annotations.SerializedName;
  */
 public class HighSchool {
 
+    @SerializedName("id")
+    private int mId;
+
     @SerializedName("name")
     private String mName;
 
@@ -16,10 +19,15 @@ public class HighSchool {
     @SerializedName("after_events")
     private int mAfterEvents;
 
-    public HighSchool(String name, int beforeEvents, int afterEvents) {
+    public HighSchool(int id, String name, int beforeEvents, int afterEvents) {
+        mId = id;
         mName = name;
         mBeforeEvents = beforeEvents;
         mAfterEvents = afterEvents;
+    }
+
+    public int getId() {
+        return mId;
     }
 
     public String getName() {
